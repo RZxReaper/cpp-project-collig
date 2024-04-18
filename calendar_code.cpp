ebc0d4ac97d96e0415420b50021de41a26404bcf
SukrutaN <156003785+SukrutaN@users.noreply.github.com>
2024-04-18 10:41:18 +0530

    Update calendar_code.cpp

--- calendar_code.cpp
+++ calendar_code.cpp
@@ -1,18 +1,23 @@
 // Online C++ compiler to run C++ program online
 #include <iostream>
+#include <bits/stdc++.h>
+#include <algorithm>
 using namespace std;
 
-const string days="mon  tue  wed  thu  fri  sat sun";
+const string days="Mon  Tue  Wed  Thu  Fri  Sat Sun";
 const string months[12]={"January","February","March","April","May","June","July","August","September","October","November","December"};
 const int t[] = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
 class calendar{
+    int n;
     int day;
     int year;
     int month_number;
     string month_name;
     int f_day;
     int day_number;
-    int reminder;
+    int event_dates[31];
+    int unsorted_event_dates[31];
+    string event_names[31];
 public:
 calendar(int d,int m,int y){
     day=d;
@@ -22,7 +27,7 @@ calendar(int d,int m,int y){
     int ytemp=year;
     ytemp-=month_number<3;
     f_day=( ytemp + ytemp/4 - ytemp/100 + ytemp/400 + t[month_number-1] + 1) % 7;
-    switch(month_number-1){
+    switch(month_number){
         case 0:
         case 2:
         case 4:
@@ -37,31 +42,55 @@ calendar(int d,int m,int y){
     }
 
 }
-    void set_rem(){cout<<"enter reminder date for current month"<<endl;cin>>reminder;}
-    void disp(){
-        cout<<f_day<<endl;
+    void set_rem(){
+        cout<<"Enter number of events to be added in the current month: "<<endl;cin>>n;
+        cout<<"Enter the details: "<<endl;
+        for(int i=0;i<n;i++)
+        {
+            cout<<"Enter date of event "<<(i+1)<<": "<<endl;
+            cin>>event_dates[i];
+            cout<<"Enter the name of event: "<<(i+1)<<": "<<endl;
+            cin>>event_names[i];
+        }
+        copy (event_dates, event_dates+n, unsorted_event_dates);
+        sort(event_dates,event_dates + n);
+    }
+    void disp()
+    {
         cout<<"---------------"<<month_name<<"---------------"<<endl;
         cout<<days<<endl;
-        if (f_day-1==-1){f_day=7;}
-        for(int i=0;i<f_day-1;i++){
+        for(int i=0;i<f_day-1;i++)
+        {
             cout<<"     ";
         }
         f_day=f_day-1;
-        for(int i=1;i<=day_number;i++){
-            if (f_day==7){cout<<endl;f_day=0;}
-            if (i==reminder){
-                cout<<"R    ";
-            }
-            else{
+        int event_no=0;
+        for(int i=1;i<=day_number;i++)
+        {
+            if (i==event_dates[event_no])
+                {
+                   cout<<"R    ";
+                   event_no++;
+                }
+            else
+            {
             cout<<i;
             string spacing = (i/10==0) ? "    " : "   ";
             cout<<spacing;
             }
             f_day++;
+            if (f_day==7){cout<<endl;f_day=0;}
         }
     }
-
-    
+    void event_disp()
+    {
+        cout<<endl<<"All the Saved Events: "<<endl;
+        for(int i=0;i<n;i++)
+        {
+            cout<<"Event "<<(i+1)<<" Name: "<<event_names[i]<<endl;
+            cout<<"Event "<<(i+1)<<" Date: "<<unsorted_event_dates[i]<<endl;
+        }
+    }
 };
 
 class UserLogin
@@ -104,17 +133,16 @@ public:
     }
 };
 
-int main() {
+int main()
+{
     UserLogin user;
     if(user.Login()==1)
     {
-        cout<<"Enter day, month and year in format dd mm yy of the calendar you want to check"<<endl;
-        int dd,mm,yyyy;
-        cin>>dd>>mm>>yyyy;
-        calendar c(dd,mm,yyyy);
+        //here the rest of the code begins.
+        calendar c(4,4,2024);
         c.set_rem();
         c.disp();
+        c.event_disp();
     }
-
     return 0;
 }

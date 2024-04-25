8114c3f42e0f9cfd8ec5e25bc4c167d3bbba8bfd
RZxReaper <86017539+RZxReaper@users.noreply.github.com>
2024-04-25 19:17:22 +0530

    Update Events.cpp

--- Events.cpp
+++ Events.cpp
@@ -1,6 +1,7 @@
 // Online C++ compiler to run C++ program online
 #include <iostream>
 #include <bits/stdc++.h>
+#include <algorithm>
 using namespace std;
 
 const string days="Mon  Tue  Wed  Thu  Fri  Sat Sun";
@@ -8,24 +9,23 @@ const string months[12]={"January","February","March","April","May","June","July
 const int t[] = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
 class calendar{
     int n;
-    int day;
     int year;
     int month_number;
     string month_name;
     int f_day;
     int day_number;
-    int reminder;
-    int dates[31];
+    int event_dates[31];
+    int unsorted_event_dates[31];
+    string event_names[31];
 public:
-calendar(int d,int m,int y){
-    day=d;
+calendar(int m,int y){
     month_number=m;
     month_name=months[month_number-1];
     year=y;
     int ytemp=year;
     ytemp-=month_number<3;
     f_day=( ytemp + ytemp/4 - ytemp/100 + ytemp/400 + t[month_number-1] + 1) % 7;
-    switch(month_number){
+    switch(month_number-1){
         case 0:
         case 2:
         case 4:
@@ -38,18 +38,28 @@ calendar(int d,int m,int y){
         case 10:
         case 8: day_number=30; break;
     }
+    if(month_number-1==1)
+    {
+        if((year%4==0 && year%100!=0)  || year%400==0)
+            day_number=29;
+        else
+            day_number=28;
+
+    }
 
 }
     void set_rem(){
         cout<<"Enter number of events to be added in the current month: "<<endl;cin>>n;
-        //int dates[n];
         cout<<"Enter the details: "<<endl;
         for(int i=0;i<n;i++)
         {
             cout<<"Enter date of event "<<(i+1)<<": "<<endl;
-            cin>>dates[i];
+            cin>>event_dates[i];
+            cout<<"Enter the name of event: "<<(i+1)<<": "<<endl;
+            cin>>event_names[i];
         }
-        sort(dates,dates + n);
+        copy (event_dates, event_dates+n, unsorted_event_dates);
+        sort(event_dates,event_dates + n);
     }
     void disp()
     {
@@ -63,7 +73,7 @@ calendar(int d,int m,int y){
         int event_no=0;
         for(int i=1;i<=day_number;i++)
         {
-            if (i==dates[event_no])
+            if (i==event_dates[event_no])
                 {
                    cout<<"R    ";
                    event_no++;
@@ -76,6 +86,26 @@ calendar(int d,int m,int y){
             }
             f_day++;
             if (f_day==7){cout<<endl;f_day=0;}
+        }
+    }
+    void event_disp()
+    {
+        int pos[n];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(event_dates[i]==unsorted_event_dates[j])
+                {
+                   pos[i]=j;
+                }
+            }
+        }
+        cout<<endl;
+        for(int i=0;i<n;i++)
+        {
+            cout<<"Event "<<(i+1)<<" Name: "<<event_names[pos[i]]<<endl;
+            cout<<"Event "<<(i+1)<<" Date: "<<event_dates[i]<<endl;
         }
     }
 };
@@ -125,10 +155,12 @@ int main()
     UserLogin user;
     if(user.Login()==1)
     {
-        //here the rest of the code begins.
-        calendar c(4,4,2024);
+        int m,y;
+        cout<<"Enter Year and Month required: "<<endl;cin>>y>>m;
+        calendar c(m,y);
         c.set_rem();
         c.disp();
+        c.event_disp();
     }
     return 0;
 }
